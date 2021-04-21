f07b28b1830dc40f256516ad1bb0666821391b48
novanyudha <digovinn@gmail.com>
2021-04-21 22:04:34 +0700

    tugas3

---
+++ javascript.js
@@ -0,0 +1,85 @@
+
+// soal nomer 1
+let soal = 'jawaban nomer 1'
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+let output = word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh
+
+console.log(soal);
+console.log(output);
+
+
+let sepasi = '     '
+console.log(sepasi);
+
+
+//soal nomer 2
+let soal1 = 'jawaban nomer 2'
+let word2 = 'wow JavaScript is so cool';
+let FirstWord = word2[0] + word2[1] + word2[2];
+let secondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[12]+ word2[13]; // do your own!
+let thirdWord = word2[15] + word2[16]; // do your own!
+let fourthWord = word2[18] + word2[19]; // do your own!
+let fifthWord = word2[21] + word2[22] + word2[23] + word2[24]; // do your own!
+
+console.log(soal1);
+console.log('First Word: ' + FirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+
+let sepasi1 = '     '
+console.log(sepasi1);
+
+
+//sooal nomer 3
+let soal2 = 'jawaban nomer 3'
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let secondWord3 = word3.substring(4, 14); // do your own!
+let thirdWord3 = word3.substring(15, 17); // do your own!
+let fourthWord3 = word3.substring(18, 20); // do your own!
+let fifthWord3 = word3.substring(21, 25); // do your own!
+
+console.log(soal2);
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
+
+
+let sepasi2 = '     '
+console.log(sepasi2);
+
+
+//soal nomer 4
+let soal3 = 'jawaban nomer 4'
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+let secondWord4 = word4.substring(4, 14); // do your own!
+let thirdWord4 = word4.substring(15, 17) // do your own!
+let fourthWord4 = word4.substring(18, 20); // do your own!
+let fifthWord4 = word4.substring(21, 25); // do your own!
+
+let firstWordLength = exampleFirstWord4.length;
+let secondtWordLength = secondWord4.length;
+let thirdWordLength = thirdWord4.length
+let fourthWordLength = fourthWord4.length
+let fifthWordLength = fifthWord4.length
+// create new variables around here
+
+console.log(soal3);
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondtWordLength);
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength);
