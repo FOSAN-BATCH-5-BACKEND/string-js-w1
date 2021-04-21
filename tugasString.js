4ab2babb814d819f94f51c8d8a15b5188475396f
athfal-r <mathfalrizki12@gmail.com>
2021-04-21 21:55:52 +0700

    Tugas 4

--- tugasString.js
+++ tugasString.js
@@ -26,17 +26,40 @@
 // console.log('Fifth Word: ' + fifthWord);
 
 //TUGAS 3
-let word3 = 'wow JavaScript is so cool';
-let exampleFirstWord3 = word3.substring(0, 3);
-let secondWord3 = word3.substring(4, 14); 
-let thirdWord3 = word3.substring(15, 17); 
-let fourthWord3 = word3.substring(18, 20); 
-let fifthWord3 = word3.substring(21, 25);
+// let word3 = 'wow JavaScript is so cool';
+// let exampleFirstWord3 = word3.substring(0, 3);
+// let secondWord3 = word3.substring(4, 14); 
+// let thirdWord3 = word3.substring(15, 17); 
+// let fourthWord3 = word3.substring(18, 20); 
+// let fifthWord3 = word3.substring(21, 25);
 
-console.log('First Word: ' + exampleFirstWord3);
-console.log('Second Word: ' + secondWord3);
-console.log('Third Word: ' + thirdWord3);
-console.log('Fourth Word: ' + fourthWord3);
-console.log('Fifth Word: ' + fifthWord3);
+// console.log('First Word: ' + exampleFirstWord3);
+// console.log('Second Word: ' + secondWord3);
+// console.log('Third Word: ' + thirdWord3);
+// console.log('Fourth Word: ' + fourthWord3);
+// console.log('Fifth Word: ' + fifthWord3);
+
+//TUGAS 4
+let word4 = 'wow JavaScript is so cool';
+
+let exampleFirstWord4 = word4.substring(0, 3);
+let secondWord4 = word4.substring(4, 14); 
+let thirdWord4 = word4.substring(15, 17); 
+let fourthWord4 = word4.substring(18, 20); 
+let fifthWord4 = word4.substring(21, 25);
+
+let firstWordLength = exampleFirstWord4.length;
+let secondWordLength = secondWord4.length;
+let thirdWordLength = thirdWord4.length;
+let fourthWordLength = fourthWord4.length;
+let fifthWordLength = fifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength);
+
+
 
 
