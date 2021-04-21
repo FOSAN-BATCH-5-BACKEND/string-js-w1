44b97020c3d1303fdf6d51e1383e5a95fc76424b
athfal-r <mathfalrizki12@gmail.com>
2021-04-21 21:44:07 +0700

    Tugas 2

--- tugasString.js
+++ tugasString.js
@@ -1,13 +1,27 @@
 
 //TUGAS 1
-let word = 'JavaScript';
-let second = 'is';
-let third = 'awesome';
-let fourth = 'and';
-let fifth = 'I';
-let sixth = 'love';
-let seventh = 'it!';
+// let word = 'JavaScript';
+// let second = 'is';
+// let third = 'awesome';
+// let fourth = 'and';
+// let fifth = 'I';
+// let sixth = 'love';
+// let seventh = 'it!';
 
-let gabung = word +" "+ second +" "+ third +" "+ fourth +" "+ fifth +" "+ sixth +" "+ seventh ;
-console.log(gabung) 
+// let gabung = word +" "+ second +" "+ third +" "+ fourth +" "+ fifth +" "+ sixth +" "+ seventh ;
+// console.log(gabung) 
+
+//TUGAS 2
+let word = 'wow JavaScript is so cool';
+let exampleFirstWord = word[0] + word[1] + word[2];
+let secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13];
+let thirdWord = word[15] + word[16] ;
+let fourthWord = word[18] + word[19] ;
+let fifthWord = word[21] + word[22] + word[23] + word[24];
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
 
